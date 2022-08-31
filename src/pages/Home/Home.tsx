2bd2ecb1a3a96bd7d7d35d51111742b57704f7df
Jonas4884 <hei.jonatana@gmail.com>
2022-08-31 08:46:32 +0300

    initialize project

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,40 +1,68 @@
-import React from 'react';
-import { Avatar,
+import { ReactNode } from 'react';
+import {
   Box,
-  Button,
   Flex,
+  Avatar,
+  Link,
+  Button,
   Menu,
   MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
   Stack,
-  useColorModeValue
+  useColorMode,
+  Center,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from '../../common/components';
 
-// a boilerplate -uh
-export const Home = () => (
-  <Box bg={useColorModeValue('gray.100', 'blackAlpha.100')} px={4}>
-    <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-      <Box>Taas template</Box>
+export const Home = ()=>{
+  return (
+      <>
+        <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+          <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+            <Box>Kaly'Naka</Box>
 
-      <Flex alignItems={'center'}>
-        <Stack direction={'row'} spacing={7}>
-          <ColorModeSwitcher />
-
-          <Menu onOpen={() => alert('not implemented !')}>
-            <MenuButton
-              as={Button}
-              rounded={'full'}
-              variant={'link'}
-              cursor={'pointer'}
-              minW={0}>
-              <Avatar
-                size={'sm'}
-                src={'https://avatars.dicebear.com/api/male/username.svg'}
-              />
-            </MenuButton>
-          </Menu>
-        </Stack>
-      </Flex>
-    </Flex>
-  </Box>
-)
+            <Flex alignItems={'center'}>
+              <Stack direction={'row'} spacing={7}>
+              <ColorModeSwitcher/>
+                <Menu>
+                  <MenuButton
+                      as={Button}
+                      rounded={'full'}
+                      variant={'link'}
+                      cursor={'pointer'}
+                      minW={0}>
+                    <Avatar
+                        size={'sm'}
+                        src={'https://avatars.dicebear.com/api/male/username.svg'}
+                    />
+                  </MenuButton>
+                  <MenuList alignItems={'center'}>
+                    <br />
+                    <Center>
+                      <Avatar
+                          size={'2xl'}
+                          src={'https://avatars.dicebear.com/api/male/username.svg'}
+                      />
+                    </Center>
+                    <br />
+                    <Center>
+                      <p>Username</p>
+                    </Center>
+                    <br />
+                    <MenuDivider />
+                    <MenuItem>Your Servers</MenuItem>
+                    <MenuItem>Account Settings</MenuItem>
+                    <MenuItem>Logout</MenuItem>
+                  </MenuList>
+                </Menu>
+              </Stack>
+            </Flex>
+          </Flex>
+        </Box>
+      </>
+  );
+}
